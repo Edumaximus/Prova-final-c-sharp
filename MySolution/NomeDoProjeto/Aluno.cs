be76a4a584e7893776e736a68758a367100c35dc
Eduardo Zaduski <eduardozaduski@gmail.com>
2024-07-12 09:33:07 -0300

    Criação do react e db

---
+++ MySolution/NomeDoProjeto/Aluno.cs
@@ -0,0 +1,10 @@
+public class Aluno
+{
+	public int Id { get; set; }
+	public string Nome { get; set; }
+	public decimal Altura { get; set; }
+
+    public decimal Quilos {get;set;}
+
+    public decimal Imc {get;set;}
+}
